ecd67ba15aa42f59f9d0525ae050f738c2ea58ee
m192920@corpr.bradesco.com.br <mario.h.s.santos@ibm.com>
2020-05-10 19:04:07 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+
+void main() {
+  runApp(SpaceShooterGame().widget);
+}
+
+class SpaceShooterGame extends Game{
+
+  @override
+  void update(double dt){
+
+  }
+
+  @override
+  void render(Canvas canvas){
+
+  }
+}
+
